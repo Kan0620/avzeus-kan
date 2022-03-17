fc2aa0eaff78260598aac3c1855efc5f595a86b0
中原寛 <nakaharakan@nakaharahiroshinoMacBook-puro.local>
2022-03-17 22:32:43 +0900

    FIX: starlette

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from fastapi import FastAPI
 #from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from api.api import api_router
 from api.heartbeat import heartbeat_router
 from core.config import settings
 from core.event_handler import start_app_handler, stop_app_handler
 
-app = FastAPI(title=settings.PROJECT_NAME)
 origins = [
     "http://localhost:3001/",
     "http://localhost:3001"
 ]
+middleware = [Middleware(CORSMiddleware, allow_origins=origins)]
+
+app = FastAPI(middleware=middleware, title=settings.PROJECT_NAME)
+
 
 app.include_router(heartbeat_router)
 app.include_router(api_router, prefix=settings.API_V1_STR, tags=["ML API"])
 
 app.add_event_handler("startup", start_app_handler(app, settings.MODEL_PATH))
 app.add_event_handler("shutdown", stop_app_handler(app))
-
+"""
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -25,7 +29,7 @@ app.add_middleware(
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["Content-Type"],
 )
-"""
+
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
