5fecab609ccd65566d8055bd71c164a4ae489824
中原寛 <nakaharakan@nakaharahiroshinoMacBook-puro.local>
2022-03-17 20:04:28 +0900

    ADD: cors

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from api.api import api_router
 from api.heartbeat import heartbeat_router
 from core.config import settings
 from core.event_handler import start_app_handler, stop_app_handler
 
 app = FastAPI(title=settings.PROJECT_NAME)
-
+origins = [
+    "http://localhost:3001/",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(heartbeat_router)
 app.include_router(api_router, prefix=settings.API_V1_STR, tags=["ML API"])
 
