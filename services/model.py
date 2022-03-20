3d7c369f5302ccdf08d27c51eb8c550fb29cfe39
中原寛 <nakaharakan@nakaharahiroshinoMacBook-puro.local>
2022-03-20 19:31:13 +0900

    NODIFIED: main.py

--- services/model.py
+++ services/model.py
@@ -45,22 +45,26 @@ class MLModel(BaseMLModel):
 
         
     def cut(self, input_text: str) -> dict:
-        img = Image.open(BytesIO(base64.b64decode(input_text)))
-        with torch.no_grad():
-            #img.save("input.png")
-            img = self.mtcnn(img, "img.png")
-        is_face = True
-        if str(img) == "None":
-            is_face = False
-        else:
-            buffer = BytesIO()
-            img = transforms.functional.to_pil_image(img)	
-            img.save(buffer, format="png")
-            img = base64.b64encode(buffer.getvalue()).decode("utf-8").replace("'", "")
-            out_img = Image.open(BytesIO(base64.b64decode(img)))
-            #out_img.save("output.png")
+        try:
+            img = Image.open(BytesIO(base64.b64decode(input_text)))
+            with torch.no_grad():
+                #img.save("input.png")
+                img = self.mtcnn(img, "img.png")
+            is_face = True
+            if str(img) == "None":
+                is_face = False
+            else:
+                buffer = BytesIO()
+                img = transforms.functional.to_pil_image(img)	
+                img.save(buffer, format="png")
+                img = base64.b64encode(buffer.getvalue()).decode("utf-8").replace("'", "")
+                out_img = Image.open(BytesIO(base64.b64decode(img)))
+                #out_img.save("output.png")
 
-        return {"is_face": is_face, "img": str(img)}
+            return {"is_face": is_face, "img": str(img)}
+
+        except:
+            return {"is_face": False, "img": "error"}
 
     def predict(self, input_text: str) -> dict:
         img = Image.open(BytesIO(base64.b64decode(input_text)))
