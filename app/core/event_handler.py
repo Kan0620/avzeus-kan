9dfa85a88af7b412e9a036bdc7c27263cb8f46ba
kan <kantasoworld0620@gmail.com>
2022-08-07 05:20:23 +0900

    feature: mov-rec loop

--- app/core/event_handler.py
+++ app/core/event_handler.py
@@ -15,18 +15,23 @@ def _startup_model(app: FastAPI, model_path: str) -> None:
     URL += ("&affiliate_id=" + os.environ["AFFILIATE_ID"])
     mov_ids = []
     mov_dict = {}
+    print("ml ok")
     for i in range(1):
         r = requests.get(URL + f"&offset={i+1}")
         for item in r.json()["result"]["items"]:
             if ("imageURL" in item.keys()) and ("sampleMovieURL" in item.keys())\
                 and ("title" in item.keys())and ("content_id" in item.keys()):
                     if ("large" in item["imageURL"].keys()) and ("size_720_480" in item["sampleMovieURL"].keys()):
-                        mov_ids.append(item["content_id"])
-                        mov_dict[item["content_id"]] = {
-                            "title": item["title"],
-                            "imageURL": item["imageURL"]["large"],
-                            "movieURL": item["sampleMovieURL"]["size_720_480"],
-                        }
+                        content_id = item["content_id"]
+                        url = f"https://cc3001.dmm.co.jp/litevideo/freepv/{content_id[0]}/{content_id[:3]}/{content_id}/{content_id}_dmb_w.mp4"
+                        status_code = requests.get(url).status_code
+                        if str(status_code) == "200":
+                            mov_ids.append(item["content_id"])
+                            mov_dict[item["content_id"]] = {
+                                "title": item["title"],
+                                "imageURL": item["imageURL"]["large"],
+                                "movieURL": item["sampleMovieURL"]["size_720_480"],
+                            }
     app.state.mov_id_set = set(mov_ids)
     app.state.mov_dict = mov_dict
 
